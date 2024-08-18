80ccbb46c5ad60186d9288190716dfdd266ad55d
Anmol parihar <121367744+Anmolp23@users.noreply.github.com>
2024-08-18 13:02:12 +0530

    Update dbConnection.js

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -1,7 +1,13 @@
 import mongoose from 'mongoose';
 
 export const dbConnection=()=>{
-    mongoose.connect(process.env.MONGO_URL)
+    mongoose.connect(process.env.MONGO_URL,{
+    dbName:"job_application",
+    useNewUrlParser: true,       // Use the new URL string parser
+    useUnifiedTopology: true,    // Use the new Server Discover and Monitoring engine
+    serverSelectionTimeoutMS: 30000,  // Connection timeout in milliseconds
+    socketTimeoutMS: 45000,      
+    })
     .then(()=>{
         console.log("connected to database")
     })
